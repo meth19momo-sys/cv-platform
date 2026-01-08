0e3fbb1d8468a96ccd3862359161744714c756a5
meth19momo-sys <meth19momo@gmail.com>
2026-01-07 18:08:02 -0800

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur notre plateforme de création de CV</h1>
+      <p>
+        Cette application vous permet de créer des curriculum vitae personnalisés,
+        de télécharger des modèles professionnels, de consulter des tutoriels et
+        de découvrir nos services de conception graphique.
+      </p>
+      <p>
+        Connectez-vous pour gérer vos CV en toute sécurité grâce à Keycloak et
+        profitez d'une base de données MongoDB Atlas pour stocker vos documents.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
